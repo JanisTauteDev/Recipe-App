6f6f0061eeb45542c5b89874dc16d0e57b3d0136
Janis Taute <taute.janis@gmail.com>
2026-02-22 11:33:48 +0100

    feat: add recipe detail placeholder with link back to list

--- src/app/features/recipes/recipe-detail/recipe-detail.ts
+++ src/app/features/recipes/recipe-detail/recipe-detail.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css',
 })
 export class RecipeDetail {
+  id: number;
 
+  constructor(private route: ActivatedRoute){ //ActivatedRoute -> Zugriff auf die aktuelle Route
+    const idParam = this.route.snapshot.paramMap.get('id'); // snapshot.paramMap.get('id') liest id aus /recipe/id, später: Observable Stream
+    this.id = Number(idParam);  //Number() -> macht aus dem String "7" die Zahl 7
+  }
 }
